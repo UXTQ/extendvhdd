420a59305f609d2e4f65ba584e5292b3072aee5e
UXTQ <113867077+UXTQ@users.noreply.github.com>
2020-12-25 09:38:53 +0000

    Pontoon: Update Punjabi (pa-IN) localization of Fundraising

--- benchmark_tools/src/persisting_hasher.rs
+++ benchmark_tools/src/persisting_hasher.rs
@@ -2,4 +2,5 @@ use std::convert::TryInto;
 use std::fs::File;
 use std::hash::{Hasher, BuildHasher};
 use std::io::{BufWriter, Write};
-use std::sy
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicU64, Orderin
