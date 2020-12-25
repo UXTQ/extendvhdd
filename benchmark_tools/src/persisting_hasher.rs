65041c246ae1edecc5134bcede92eef98868924c
UXTQ <113867077+UXTQ@users.noreply.github.com>
2020-12-25 10:08:05 +0000

    net: vlan,ethtool: netdev_features_t is more than 32 bit

--- benchmark_tools/src/persisting_hasher.rs
+++ benchmark_tools/src/persisting_hasher.rs
@@ -3,4 +3,7 @@ use std::fs::File;
 use std::hash::{Hasher, BuildHasher};
 use std::io::{BufWriter, Write};
 use std::sync::{Arc, Mutex};
-use std::sync::atomic::{AtomicU64, Orderin
+use std::sync::atomic::{AtomicU64, Ordering};
+use once_cell::sync::OnceCell;
+use std::process::id;
+
