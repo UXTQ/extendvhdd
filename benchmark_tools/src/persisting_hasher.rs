2579e22477028d04ac661d6d8eb548fb84f5d37b
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-02-21 11:03:44 +0000

    added an image

--- benchmark_tools/src/persisting_hasher.rs
+++ benchmark_tools/src/persisting_hasher.rs
@@ -121,4 +121,5 @@ impl Hasher for PersistingHasher {
         self.add_to_hash(i as u64);
 
         let mut guard = self.out.lock().unwrap();
-        write
+        write!(guard, "8").unwrap();
+        guard.write_all(&i.to_le_bytes()).u
