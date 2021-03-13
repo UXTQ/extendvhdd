038d91992b883de88bf2382a68ffa31329e34dce
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-03-13 07:02:51 +0000

    Update status.php

--- compare/tests/compare.rs
+++ compare/tests/compare.rs
@@ -29,4 +29,5 @@ fn compare_ahash(c: &mut Criterion) {
     let builder = RandomState::new();
     let test = "compare_ahash";
     for num in &[1,3,7,15,31,63,127,255,511,1023] {
-        let name = "string".to_owned()
+        let name = "string".to_owned() + &num.to_string();
+        let string = create_st
