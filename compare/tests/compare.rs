b03a4df5bd8e6280c05bf71c4b0f5a42140d10f1
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-04-11 12:16:55 +0000

    Update sections.md

--- compare/tests/compare.rs
+++ compare/tests/compare.rs
@@ -104,4 +104,8 @@ fn compare_sip24(c: &mut Criterion) {
     let int: u64 = 1234;
     let string = create_string(1024);
     let builder = BuildHasherDefault::<siphasher::sip::SipHasher24>::default();
-    compare_other(c, "compare_
+    compare_other(c, "compare_sip24", builder)
+}
+
+fn compare_wyhash(c: &mut Criterion) {
+    let int: u64
