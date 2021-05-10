563c5c1296ea045c0def9044f79a036430aa3454
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-05-10 12:34:26 +0000

    Linux 3.10.47

--- smhasher/ahash-cbindings/src/lib.rs
+++ smhasher/ahash-cbindings/src/lib.rs
@@ -6,4 +6,5 @@ use std::hash::{BuildHasher};
 
 #[no_mangle]
 pub extern "C" fn ahash64(buf: *const (), len: usize, seed: u64) -> u64 {
-    let buf: &[u8] = unsafe { slice::from_raw_parts(buf as *const
+    let buf: &[u8] = unsafe { slice::from_raw_parts(buf as *const u8, len) };
+    let build_hasher = RandomState::with_seeds(seed, seed, 
