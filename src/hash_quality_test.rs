84d46ec53ce6c83e9a6e6f2b2d608942f69a142e
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-07-30 17:52:37 +0000

    ARM: kernel: enhance MPIDR macro definitions

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -1,4 +1,5 @@
 use core::hash::{Hash, Hasher};
 use std::collections::HashMap;
 
-fn assert_sufficiently_different(a: u64, b: u64, tolerance: i3
+fn assert_sufficiently_different(a: u64, b: u64, tolerance: i32) {
+    let (same_byte_count, same_nibble_count) = count_
