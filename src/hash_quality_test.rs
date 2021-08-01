f38ab135216184bd3d1693355ecdb278433461dc
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-08-01 17:10:32 +0000

    v2.4.5

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -2,4 +2,5 @@ use core::hash::{Hash, Hasher};
 use std::collections::HashMap;
 
 fn assert_sufficiently_different(a: u64, b: u64, tolerance: i32) {
-    let (same_byte_count, same_nibble_count) = count_
+    let (same_byte_count, same_nibble_count) = count_same_bytes_and_nibbles(a, b);
+    assert!(same_byte_count <= toler
