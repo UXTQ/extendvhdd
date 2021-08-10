a1a9473762a3e1ba29213c304ae6be6d91e46467
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-08-10 15:22:52 +0000

    mmc: mmci: fix an ages old detection error

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -6,4 +6,7 @@ fn assert_sufficiently_different(a: u64, b: u64, tolerance: i32) {
     assert!(same_byte_count <= tolerance, "{:x} vs {:x}: {:}", a, b, same_byte_count);
     assert!(
         same_nibble_count <= tolerance * 3,
-        "{:x} vs {:x}: 
+        "{:x} vs {:x}: {:}",
+        a,
+        b,
+        same_nibble_coun
