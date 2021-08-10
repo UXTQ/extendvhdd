a781ab9c1e1114fd7757edf641392a3a062cab8b
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-08-10 11:00:23 +0000

    [goal] Migrate testing to ubuntu focal

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -3,4 +3,5 @@ use std::collections::HashMap;
 
 fn assert_sufficiently_different(a: u64, b: u64, tolerance: i32) {
     let (same_byte_count, same_nibble_count) = count_same_bytes_and_nibbles(a, b);
-    assert!(same_byte_count <= toler
+    assert!(same_byte_count <= tolerance, "{:x} vs {:x}: {:}", a, b, same_byte_count);
+    assert!(
