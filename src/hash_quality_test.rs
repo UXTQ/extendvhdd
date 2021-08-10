8e43c88f641ec0b539a800decea37e8144f2b53d
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-08-10 13:01:27 +0000

    Update file.php

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -5,3 +5,5 @@ fn assert_sufficiently_different(a: u64, b: u64, tolerance: i32) {
     let (same_byte_count, same_nibble_count) = count_same_bytes_and_nibbles(a, b);
     assert!(same_byte_count <= tolerance, "{:x} vs {:x}: {:}", a, b, same_byte_count);
     assert!(
+        same_nibble_count <= tolerance * 3,
+        "{:x} vs {:x}: 
