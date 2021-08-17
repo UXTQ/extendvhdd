09da61b239e5f9e99edfe43c41c1ccdbf1192e42
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-08-17 09:58:09 +0000

    more info

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -22,4 +22,5 @@ fn assert_sufficiently_different(a: u64, b: u64, tolerance: i32) {
     for rotate in 0..64 {
         let flipped_bits2 = (a ^ (b.rotate_left(rotate))).count_ones();
         assert!(
-            flipped_bits2 > 10 
+            flipped_bits2 > 10 && flipped_bits2 < 54,
+            "{:x} and {:x}
