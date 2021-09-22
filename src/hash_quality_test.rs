3a79579f1633a8590d917aa485679ba868508832
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-09-22 18:14:29 +0000

    Update some links

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -78,4 +78,6 @@ fn test_no_full_collisions<T: Hasher>(gen_hash: impl Fn() -> T) {
             middle.to_vec()
         };
         let mut hasher = gen_hash();
-        hasher.w
+        hasher.write(&array);
+        let hash = hasher.finish();
+        
