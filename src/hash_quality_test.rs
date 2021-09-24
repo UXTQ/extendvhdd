deec9668b3e47af9b02fcaae79adf6f142d3d7f4
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-09-24 08:43:41 +0000

    Corrected indentation

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -81,4 +81,6 @@ fn test_no_full_collisions<T: Hasher>(gen_hash: impl Fn() -> T) {
         hasher.write(&array);
         let hash = hasher.finish();
         if let Some(value) = map.get(&hash) {
-            asser
+            assert_eq!(
+                value, &array,
+                "Found
