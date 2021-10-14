ac3cb6050c1b34c33e77a0a149f09a09964e7c48
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-10-14 14:09:47 +0000

    fix logs

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -113,4 +113,6 @@ fn test_keys_change_output<T: Hasher>(constructor: impl Fn(u128, u128) -> T) {
 fn test_input_affect_every_byte<T: Hasher>(constructor: impl Fn(u128, u128) -> T) {
     let base = hash_with(&0, constructor(0, 0));
     for shift in 0..16 {
-        let mut alternitives = 
+        let mut alternitives = vec![];
+        for v in 0..256 {
+            let input = (v as u128
