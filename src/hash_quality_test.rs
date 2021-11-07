0635e6ab6caa95414914669f55e7fa2c9fc6d8a3
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-11-07 17:01:40 +0000

    Update Report.md

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -161,4 +161,6 @@ fn assert_each_byte_differs(num: u64, base: u64, alternitives: Vec<u64>) {
 fn test_finish_is_consistent<T: Hasher>(constructor: impl Fn(u128, u128) -> T) {
     let mut hasher = constructor(1, 2);
     "Foo".hash(&mut hasher);
-    let a = hasher.fini
+    let a = hasher.finish();
+    let b = hasher.finish();
+    assert_eq!
