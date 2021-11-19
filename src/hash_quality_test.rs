862c382c86ae282b03e6e4f1a981c77f309dbb97
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-11-19 15:14:36 +0000

    workqueue: change BUG_ON() to WARN_ON()

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -172,4 +172,6 @@ fn test_single_key_bit_flip<T: Hasher>(constructor: impl Fn(u128, u128) -> T) {
         let mut b = constructor(0, 1 << bit);
         let mut c = constructor(1 << bit, 0);
         "1234".hash(&mut a);
-        "1234".hash(&mut
+        "1234".hash(&mut b);
+        "1234".hash(&mut c);
+        assert_suffici
