c45797b598a4a1f08385182db2eee03a444f3ba7
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-11-26 08:14:37 +0000

    Update example in readme

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -175,4 +175,5 @@ fn test_single_key_bit_flip<T: Hasher>(constructor: impl Fn(u128, u128) -> T) {
         "1234".hash(&mut b);
         "1234".hash(&mut c);
         assert_sufficiently_different(a.finish(), b.finish(), 2);
-        assert_suffici
+        assert_sufficiently_different(a.finish(), c.finish(), 2);
+        as
