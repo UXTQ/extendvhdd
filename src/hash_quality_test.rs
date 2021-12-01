a295e0a387f19ea3abf7e866f68a310842816b4b
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-12-01 10:34:56 +0000

    heroku fixes

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -183,4 +183,5 @@ fn test_single_key_bit_flip<T: Hasher>(constructor: impl Fn(u128, u128) -> T) {
         "12345678".hash(&mut a);
         "12345678".hash(&mut b);
         "12345678".hash(&mut c);
-        assert_sufficiently_different(a.finish(), b.finish
+        assert_sufficiently_different(a.finish(), b.finish(), 2);
+        assert_sufficiently_different(a.finish(), c.
