41ec19882f1b126b6c0085927516e4cfe4cd6300
UXTQ <113867077+UXTQ@users.noreply.github.com>
2021-12-09 20:37:32 +0000

    Released version 3.4.1.0

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -189,4 +189,5 @@ fn test_single_key_bit_flip<T: Hasher>(constructor: impl Fn(u128, u128) -> T) {
         let mut a = constructor(0, 0);
         let mut b = constructor(0, 1 << bit);
         let mut c = constructor(1 << bit, 0);
+        "1234567812345678".hash(&mut a);
         "12
