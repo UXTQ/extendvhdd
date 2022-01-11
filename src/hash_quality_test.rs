11cbc65240a5b9f9b2ea9fc35298e849c93dd57e
UXTQ <113867077+UXTQ@users.noreply.github.com>
2022-01-11 18:20:38 +0000

    Update Welcome.md

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -226,4 +226,5 @@ fn test_no_pair_collisions<T: Hasher>(hasher: impl Fn() -> T) {
             assert_sufficiently_different(base_hash, bb, 3);
             assert_sufficiently_different(aa, ab, 3);
             assert_sufficiently_different(ab, ba, 3);
-            a
+            assert_sufficiently_different(ba, bb, 3);
+            assert_suffici
