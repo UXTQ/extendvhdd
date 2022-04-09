00e9dbf314bd81fe7fd1580d8e0ade180df999d6
UXTQ <113867077+UXTQ@users.noreply.github.com>
2022-04-09 15:30:11 +0000

    Update material.md

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -385,4 +385,5 @@ mod fallback_tests {
     #[test]
     fn fallback_keys_affect_every_byte() {
         //For fallback second key is not used in every hash.
-        #[cfg(all(
+        #[cfg(all(not(feature = "specialize"), feature = "folded_multiply"))]
+   
