38df68cc7de29702f21965570a5043b606ffdc2b
UXTQ <113867077+UXTQ@users.noreply.github.com>
2022-04-10 21:29:21 +0000

    Remove metadata.json and update application readme.

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -386,4 +386,5 @@ mod fallback_tests {
     fn fallback_keys_affect_every_byte() {
         //For fallback second key is not used in every hash.
         #[cfg(all(not(feature = "specialize"), feature = "folded_multiply"))]
-        test_keys_affect_every_byte(0, |a, b| AHasher::n
+        test_keys_affect_every_byte(0, |a, b| AHasher::new_with_keys(a ^ b, a));
+        test_keys_affect_every_byte
