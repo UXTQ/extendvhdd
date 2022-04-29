5ec3edf9d3306d00df713d0f2d7fd46632a4480a
UXTQ <113867077+UXTQ@users.noreply.github.com>
2022-04-29 20:10:52 +0000

    Fix #103

--- src/hash_quality_test.rs
+++ src/hash_quality_test.rs
@@ -413,3 +413,5 @@ mod fallback_tests {
 ///Basic sanity tests of the cypto properties of aHash.
 #[cfg(any(
     all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "aes", not(miri)),
+    all(
+        any(target_arch = "arm", target_arch = 
