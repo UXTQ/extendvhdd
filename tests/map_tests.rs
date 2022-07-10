083afd65f16a8927df021df76e27ae4319363981
UXTQ <113867077+UXTQ@users.noreply.github.com>
2022-07-10 06:42:49 +0000

    Finish 2.2.0

--- tests/map_tests.rs
+++ tests/map_tests.rs
@@ -16,4 +16,5 @@ amount, analysis, and, animal, another, answer, any, anyone, anything, appear,
 apply, approach, area, argue, arm, around, arrive, art, article, artist, as,
 ask, assume, at, attack, attention, attorney, audience, author, authority,
 available, avoid, away, baby, back, bad, bag, ball, bank, bar, base, be, beat,
-beautiful, because, bec
+beautiful, because, become, bed, before, begin, behavior, behind, believe,
+benefit, bes
