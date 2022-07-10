7b829bbd089ab7235cd151cf806165948556f207
UXTQ <113867077+UXTQ@users.noreply.github.com>
2022-07-10 22:02:28 +0000

    typo

--- tests/map_tests.rs
+++ tests/map_tests.rs
@@ -17,4 +17,5 @@ apply, approach, area, argue, arm, around, arrive, art, article, artist, as,
 ask, assume, at, attack, attention, attorney, audience, author, authority,
 available, avoid, away, baby, back, bad, bag, ball, bank, bar, base, be, beat,
 beautiful, because, become, bed, before, begin, behavior, behind, believe,
-benefit, best, better, between, beyond, big, bill, billion, bit, black, bloo
+benefit, best, better, between, beyond, big, bill, billion, bit, black, blood,
+blue, board, body, book, born, both, box, boy, break,
