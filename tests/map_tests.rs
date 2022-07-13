89d35bb378dc51d3bc36f856d427a41c1c8f9e31
UXTQ <113867077+UXTQ@users.noreply.github.com>
2022-07-13 16:24:22 +0000

    Add Finnish translation

--- tests/map_tests.rs
+++ tests/map_tests.rs
@@ -19,4 +19,5 @@ available, avoid, away, baby, back, bad, bag, ball, bank, bar, base, be, beat,
 beautiful, because, become, bed, before, begin, behavior, behind, believe,
 benefit, best, better, between, beyond, big, bill, billion, bit, black, blood,
 blue, board, body, book, born, both, box, boy, break, bring, brother, budget,
-build, building, business, but, bu
+build, building, business, but, buy, by, call, camera, campaign, can, cancer,
+candida
