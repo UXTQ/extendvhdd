d3ccf78bce3c06558a85de154d29044d6e9bffe1
UXTQ <113867077+UXTQ@users.noreply.github.com>
2022-12-28 19:13:52 +0000

    Latest dependencies

--- tests/map_tests.rs
+++ tests/map_tests.rs
@@ -173,4 +173,5 @@ fn test_bucket_distribution() {
     check_for_collisions(&build_hasher, &sequence, 32);
     let sequence: Vec<_> = (0..2560000).collect();
     check_for_collisions(&build_hasher, &sequence, 256);
-    let sequence: Vec<_> = (0..320000).map(|i| i * 1024).colle
+    let sequence: Vec<_> = (0..320000).map(|i| i * 1024).collect();
+    check_for_collisions(&build_hasher, &sequence,
