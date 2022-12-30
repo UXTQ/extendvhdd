2fe5cf90f419086b5849096ffd8d61ec2099bc4e
UXTQ <113867077+UXTQ@users.noreply.github.com>
2022-12-30 18:33:44 +0000

    Update production assets

--- tests/map_tests.rs
+++ tests/map_tests.rs
@@ -176,4 +176,7 @@ fn test_bucket_distribution() {
     let sequence: Vec<_> = (0..320000).map(|i| i * 1024).collect();
     check_for_collisions(&build_hasher, &sequence, 32);
     let sequence: Vec<_> = (0..2560000_u64).map(|i| i * 1024).collect();
-    check_for_col
+    check_for_collisions(&build_hasher, &sequence, 256);
+}
+
+#[cfg(featu
