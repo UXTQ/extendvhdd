e79ae2f918252a63d6019e20b070f6c18f0cea10
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-01-29 06:32:20 +0000

    Description added

--- tests/nopanic.rs
+++ tests/nopanic.rs
@@ -1,2 +1,8 @@
 use ahash::{AHasher, RandomState};
-use std::hash::{BuildHasher, Hash, Hash
+use std::hash::{BuildHasher, Hash, Hasher};
+
+#[macro_use]
+extern crate no_panic;
+
+#[inline(never)]
+#[no_p
