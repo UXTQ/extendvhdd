671005f8c4ec15100772eb4a13f9b4899a7f55bb
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-01-31 15:11:57 +0000

    Fixed typo in docs.

--- tests/nopanic.rs
+++ tests/nopanic.rs
@@ -6,4 +6,6 @@ extern crate no_panic;
 
 #[inline(never)]
 #[no_panic]
-fn hash_test_final(num: i32, string: &str) -
+fn hash_test_final(num: i32, string: &str) -> (u64, u64) {
+    use core::hash::Hasher;
+    let mut hasher
