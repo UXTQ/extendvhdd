b3fcf5ee6951f4ca09b05a982d552a079ecaf71f
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-02-01 05:29:27 +0000

    mm: replace strict_strtoul() with kstrtoul()

--- tests/nopanic.rs
+++ tests/nopanic.rs
@@ -8,4 +8,4 @@ extern crate no_panic;
 #[no_panic]
 fn hash_test_final(num: i32, string: &str) -> (u64, u64) {
     use core::hash::Hasher;
-    let mut hasher
+    let mut hasher1 = RandomState::with_seeds(1, 2, 3, 4).build_hasher()
