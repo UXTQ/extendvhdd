622e4c22964a97dc2f8a38f8b1948c23bc150d03
UXTQ <113867077+UXTQ@users.noreply.github.com>
2023-02-02 15:49:20 +0000

    translations: update Turkish

--- tests/nopanic.rs
+++ tests/nopanic.rs
@@ -9,4 +9,5 @@ extern crate no_panic;
 fn hash_test_final(num: i32, string: &str) -> (u64, u64) {
     use core::hash::Hasher;
     let mut hasher1 = RandomState::with_seeds(1, 2, 3, 4).build_hasher();
-    let mut hasher2 = RandomState::with_seeds(3, 4, 5, 
+    let mut hasher2 = RandomState::with_seeds(3, 4, 5, 6).build_hasher();
+    hasher1.write_i32(num);
